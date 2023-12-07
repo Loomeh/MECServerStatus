50af020ba5f9316af9c06a5eddb4839d6fcfba03
Zane Holbrooke-Jones <67561520+Loomeh@users.noreply.github.com>
2023-12-07 03:17:54 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-document.addEventListener("DOMContentLoaded", function()
-{
+document.addEventListener("DOMContentLoaded", function () {
     const apiUrl = 'https://mec-gw.ops.dice.se/jsonrpc/prod_default/prod_default/pc/api';
     const requestData = {
         jsonrpc: '2.0',
         method: 'Pamplona.getPlayerTag',
-        params:
-        {
+        params: {
             personaId: ''
         },
         id: '5dbcfaff-75ff-4860-a5dc-fe8ea06d5728'
     };
     const requestOptions = {
         method: 'POST',
-        headers:
-        {
+        headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(requestData),
@@ -24,35 +21,46 @@ document.addEventListener("DOMContentLoaded", function()
     // Add a delay of 2 seconds
     setTimeout(() =>
     {
+        makeRequest();
+        console.log("Making initial request");
+    }, 2000);
+
+    // Make call every 2 minutes
+    setInterval(() =>
+    {
+        console.log("Making a new request");
+        setStatusTextAndColor('Checking...');
+
+        setTimeout(() =>
+        {
+            makeRequest();
+        }, 2000);
+    }, 120000);
+
+    function makeRequest() {
         fetch(apiUrl, requestOptions)
-            .then(response =>
-            {
-                if (!response.ok)
-                {
+            .then(response => {
+                if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
                 }
                 return response.json();
             })
-            .then(data =>
-            {
+            .then(data => {
                 setStatusTextAndColor('Active');
             })
-            .catch(error =>
-            {
+            .catch(error => {
                 console.error('Error:', error);
                 setStatusTextAndColor('Inactive');
             });
-    }, 2000);
+    }
 
-    function setStatusTextAndColor(status)
-    {
+    function setStatusTextAndColor(status) {
         statusTextElement.textContent = `${status}`;
-        if (status === 'Active')
-        {
+        if (status === 'Active') {
             statusTextElement.style.color = 'green';
-        }
-        else
-        {
+        } else if(status === 'Checking...') {
+            statusTextElement.style.color = 'white';
+        } else {
             statusTextElement.style.color = 'red';
         }
     }
